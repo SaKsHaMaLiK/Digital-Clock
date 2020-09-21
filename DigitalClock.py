d51755fca89b5ec5f62ffae0a2b12551360a6406
Saksham Malik <54699504+SaKsHaMaLiK@users.noreply.github.com>
2020-09-21 10:34:04 +0530

    Add files via upload

---
+++ DigitalClock.py
@@ -0,0 +1,19 @@
+import time
+from tkinter import *
+root = Tk()
+root.title("Digital Clock")
+root.geometry("250x100+0+0")
+root.resizable(0, 0)
+label = Label(root, font=("Digital, Comic sans, Arial", 25, 'italic'),
+              bg="black", fg="crimson", bd=65)
+label.grid(row=0, column=1)
+
+
+def clock():
+    text_input = time.strftime("%H:%M:%S")
+    label.config(text=text_input)
+    label.after(200, clock)
+
+
+clock()
+root.mainloop()
